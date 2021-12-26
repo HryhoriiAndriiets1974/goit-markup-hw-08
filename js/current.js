3a768e6c80f686e1a5a22e0e03b42d4529992ada
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2021-12-26 12:04:29 +0200

    menu-1

--- js/current.js
+++ js/current.js
@@ -1,5 +1,4 @@
 (() => {
-<<<<<<< HEAD
   document
     .querySelector('.js-speaker-form')
     .addEventListener('submit', e => {
@@ -12,17 +11,3 @@
       e.currentTarget.reset();
     });
 })();
-=======
-    document
-      .querySelector('.js-speaker-form')
-      .addEventListener('submit', e => {
-        e.preventDefault();
-
-        new FormData(e.currentTarget).forEach((value, name) =>
-          console.log(`${name}: ${value}`),
-        );
-
-        e.currentTarget.reset();
-      });
-  })();
->>>>>>> 18b0ce60274d67e4bb51f063e6a8b9126b64fdb5
